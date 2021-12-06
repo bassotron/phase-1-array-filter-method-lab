038ee21f25d58da6bb95274411cf5ed68e38e7ab
Will Lovell <willard.lovell@icloud.com>
2021-12-05 22:40:07 -0700

    commit tree 878b229aacc2c2a569c788e350d8bdb04e78f23a

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(driverArray, driverName) {
+return driverArray.filter(iteratorWord => iteratorWord.toLowerCase() == driverName.toLowerCase() )}
+
+
+
+function fuzzyMatch(driverArray, driverName) {
+    return driverArray.filter(iteratorWord => iteratorWord.substring(0,1) == driverName.substring(0,1))}
+
+
+
+function matchName(driverArray, driverName) {
+    return driverArray.filter(iteratorWord => iteratorWord.name === driverName)
+}
